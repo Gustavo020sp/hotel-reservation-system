60a1d1876c1c32312df0a13f841fc4bc669e4717
Gustavo020sp <gustavo020sp@gmail.com>
2025-05-12 20:56:08 -0300

    first commit

---
+++ hotel_reservation_system/hotel_reservation_system/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace hotel_reservation_system
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
