0d7e519724bc6d1beef714e78683c083b4229679
Gustavo020sp <gustavo020sp@gmail.com>
2025-05-12 21:02:16 -0300

    added booking interface

---
+++ hotel_reservation_system/hotel_reservation_system/Interfaces/IBooking.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hotel_reservation_system.Interfaces
+{
+    internal interface IBooking
+    {
+
+        //methods
+        public int CalculatePrice(int days);
+        public bool IsAvailable();
+    }
+}
